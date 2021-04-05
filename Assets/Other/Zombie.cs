2251f3d978c725631f9aa37b4bc3f8d30fa9918f
Руслан Рогозин <wether@mail2000.ru>
2021-04-05 22:27:19 +0700

    + system of multi-genetical pursuit and loss of the player from sight

--- Assets/Other/Zombie.cs
+++ Assets/Other/Zombie.cs
@@ -5,19 +5,67 @@ using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
-    private AIPursuit _ai;
-    private Transform _target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _ai = new AIPursuit();
-        _ai.Agent = GetComponent<NavMeshAgent>();
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+    private bool        _isActivePursuit;
+    private bool        _isPlayerInSight;
+    private AIPursuit   _ai;
+    private Transform   _targetPos;
+    private Vector3     _lastVisitPos;
 
-    // Update is called once per frame
+    public bool IsActivePursuit { get => _isActivePursuit; }
+
+    void OnEnable()
+    {
+        _isPlayerInSight = true;
+        _isActivePursuit = false;
+        _lastVisitPos = Vector3.zero;
+        _targetPos = GameObject.FindGameObjectWithTag("Player").transform;
+        _ai = new AIPursuit
+        {
+            Agent = GetComponent<NavMeshAgent>()
+        };
+    }
+    private void SetLastVisitPos()
+    {
+        if (_isPlayerInSight)
+            _lastVisitPos = _targetPos.position;
+        if (_ai.Agent.Raycast(_targetPos.position, out _))
+            _isPlayerInSight = false;
+    } 
+    private void StoppingOnTargetLoss()
+    {
+        if (_ai.Agent.isStopped
+            && !_isPlayerInSight)
+        {
+            _isActivePursuit = false;
+        }
+    }
     void Update()
     {
-        _ai.Agent.SetDestination(_target.position);
+        if (_isActivePursuit)
+        {
+            SetLastVisitPos();
+            _ai.Agent.SetDestination(_lastVisitPos);
+            StoppingOnTargetLoss();
+        }
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            _isActivePursuit = true;
+            _isPlayerInSight = true;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Zombie zombie = (Zombie)other.gameObject.GetComponent("Zombie");
+            if (zombie.IsActivePursuit)
+            {
+                _isActivePursuit = true;
+            }
+        }
     }
 }
